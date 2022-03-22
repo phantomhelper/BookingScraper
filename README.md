c6aba8004b3691d5d5726e22840c6ae0c33b4aa1
phantomhelper <dankaonpc@gmail.com>
2022-03-22 12:58:08 +0100

    2022-03-22 DK

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # BookingScraper
+
+## Structure of single opinion
+
+|Element|Selector|Variable|
+|-------|--------|--------|
+|review|li.review_list_new_item_block|
+|review id|li.review_list_new_item_block|
+|subject||
+|pros||
+|cons||
+|score||
+|review date||
+|review language||
+|author||
+|county||
+|room type||
+|guest type||
+|staying||
--- scrapper.py
+++ scrapper.py
@@ -1,12 +1,22 @@
 from importlib.metadata import requires
+from itertools import count
+from bs4 import BeautifulSoup
 import requests
 
 
-url = "https://www.booking.com/hotel/pl/happy-tower.html"
+url = "https://www.booking.com/hotel/pl/happy-tower.html#tab-reviews"
+#url = "https://www.booking.com/hotel/pl/happy-tower.html"
+
 r = requests.get(url)
 
 if r.status_code == 200:
     print('[INFO]: Roger.')
-    print(r.text)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    print(soup)
+    reviews = soup.select("li.review_list_new_item_block")
+    print(len(reviews))
+
+
 else:
     print(f"[INFO]: Something went wrong...\nStatus code: {r.status_code}")
